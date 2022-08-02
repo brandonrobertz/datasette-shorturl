270f09ef782f3a81312591090d7ce3c42405dbe4
Brandon Roberts <brandon@bxroberts.org>
2022-08-01 17:20:06 -0700

    Add test for misconfiguration

--- tests/test_shorturl.py
+++ tests/test_shorturl.py
@@ -15,3 +15,10 @@ async def test_plugin_is_installed():
     assert response.status_code == 200
     installed_plugins = {p["name"] for p in response.json()}
     assert "datasette-shorturl" in installed_plugins
+
+
+@pytest.mark.asyncio
+async def test_plugin_500s_if_misconfigured():
+    datasette = Datasette(memory=True, metadata=None)
+    response = await datasette.client.get("/-/plugins.json")
+    assert response.status_code == 500
